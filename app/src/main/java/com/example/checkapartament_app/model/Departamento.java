7d4a750b1480165a6118cb2ff2f30911d7f65f0f
retromantics <camila.campos.soto@gmail.com>
2021-10-22 17:13:59 -0300

    Add Models Add Login Functionality

--- app/src/main/java/com/example/checkapartament_app/model/Departamento.java
+++ app/src/main/java/com/example/checkapartament_app/model/Departamento.java
@@ -1,4 +1,82 @@
 package com.example.checkapartament_app.model;
 
+import java.util.List;
+
 public class Departamento {
+    private int id;
+    private String proyecto;
+    private int nroDepartamento;
+    private String direccion;
+    private List<Terminacion> TerminacionL;
+    private int estadoGeneral ;
+
+    public Departamento() {
+    }
+
+    public Departamento(String proyecto, int nroDepartamento, String direccion, List<Terminacion> terminacionL, int estadoGeneral) {
+        this.proyecto = proyecto;
+        this.nroDepartamento = nroDepartamento;
+        this.direccion = direccion;
+        TerminacionL = terminacionL;
+        this.estadoGeneral = estadoGeneral;
+    }
+
+    public Departamento(int id, String proyecto, int nroDepartamento, String direccion, List<Terminacion> terminacionL, int estadoGeneral) {
+        this.id = id;
+        this.proyecto = proyecto;
+        this.nroDepartamento = nroDepartamento;
+        this.direccion = direccion;
+        TerminacionL = terminacionL;
+        this.estadoGeneral = estadoGeneral;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProyecto() {
+        return proyecto;
+    }
+
+    public void setProyecto(String proyecto) {
+        this.proyecto = proyecto;
+    }
+
+    public int getNroDepartamento() {
+        return nroDepartamento;
+    }
+
+    public void setNroDepartamento(int nroDepartamento) {
+        this.nroDepartamento = nroDepartamento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public List<Terminacion> getTerminacionL() {
+        return TerminacionL;
+    }
+
+    public void setTerminacionL(List<Terminacion> terminacionL) {
+        TerminacionL = terminacionL;
+    }
+
+
+    public int getEstadoGeneral() {
+        return estadoGeneral;
+    }
+
+    public void setEstadoGeneral(int estadoGeneral) {
+        this.estadoGeneral = estadoGeneral;
+    }
+
 }
--- app/src/main/java/com/example/checkapartament_app/model/User.java
+++ app/src/main/java/com/example/checkapartament_app/model/User.java
@@ -1,4 +1,43 @@
 package com.example.checkapartament_app.model;
 
+
+import android.nfc.FormatException;
+
 public class User {
+
+    private String email;
+    private String password ;
+
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) throws FormatException {
+        if(!email.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")){
+            throw new FormatException("Email Invalido");
+        }else{
+            this.email = email;
+        }
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) throws Exception {
+        if(!password.equals("123Pass" )){
+            throw new Exception("Contaseña Incorrecta");
+        }else{
+            this.password = password;
+        }
+    }
 }
